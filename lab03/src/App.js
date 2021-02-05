c441bc4152e020b05fc0cce13b0442fd4bce3743
y604499781 <54754052+y604499781@users.noreply.github.com>
2021-02-05 17:49:19 -0500

    lab 03

---
+++ lab03/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './components/homepage'
+
+function App() {
+  const fname = 'Yi';
+  return (
+    <HomePage firstName = {fname} />
+  )
+}
+
+export default App;
