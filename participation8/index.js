7842e9560112d656ab884902a2781d2715b6e953
y604499781 <54754052+y604499781@users.noreply.github.com>
2021-03-09 17:40:00 -0500

    participation 8

---
+++ participation8/index.js
@@ -0,0 +1,19 @@
+import Express from 'express';
+
+const App = Express();
+const port = 3001;
+
+//parameter defined by ':'
+//access "id" via 'req.params.id'
+App.get('/users/:id', (req, res) => {
+  res.send("The requested user ID is: " + req.params.id);
+});
+
+//respond with json object
+App.get('/names/:fname', (req, res) => {
+  res.json({firstname: req.params.fname});
+});
+
+//listens on port
+App.listen(port, () => {
+});
